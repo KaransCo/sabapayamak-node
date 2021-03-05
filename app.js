d02a9651cb607f4a15ce7f256a79dd70e78c6fd4
KaransCo <asemaneabico@gmail.com>
2021-03-06 02:44:10 +0330

    delete test

--- app.js
+++
@@ -1,9 +0,0 @@
-var sabapayamak = require("./sabapayamak")
-var api = sabapayamak.SabapayamakApi({
-    host: 'api.sabapayamak.com'
-});
-api.getToken('ghanimati', 'd9ace', '3000710901', 1,
-    function(response, status) {
-        console.log(response);
-        console.log(status);
-    });
